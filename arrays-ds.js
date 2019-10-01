3a269fefd4b0b598279cd4c42038c6ad0536c300
subodh <subodhss23@gmail.com>
2019-09-30 23:56:19 -0700

    built in method

---
+++ arrays-ds.js
@@ -0,0 +1,9 @@
+// print all N integers in A in reverse order as a single line
+// of space-separated integers.
+
+let array = [1,2,3,4,5,5];
+
+function reveseArr(array){
+    console.log(array.reverse());
+}
+reveseArr(array);
